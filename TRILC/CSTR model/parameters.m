82167c2158822beaab91eee234138595b5fc8750
JiaaaWang <139123553+JiaaaWang@users.noreply.github.com>
2024-06-17 01:32:41 +0200

    Delete TRILC directory

--- TRILC/CSTR model/parameters.m
+++
@@ -1,7 +0,0 @@
-m = 1; % input number
-p = 1; % output number
-N = 100; % data length in one trial
-M = 5; % Maximal main trial number
-U_ini_ini = 5 * ones(N, m); % input trajectory of the first trial
-Y_ini = zeros(N, p); % output trajectory of the first trial, initialize from 0
-R = 1.96 * ones(p * N, 1); % reference trajectory
