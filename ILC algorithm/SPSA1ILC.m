ecaa48481f51d5438e0840e2be56d8270aa0503b
JiaaaWang <139123553+JiaaaWang@users.noreply.github.com>
2024-06-17 14:50:14 +0200

    Update SPSA1ILC.m

--- ILC algorithm/SPSA1ILC.m
+++ ILC algorithm/SPSA1ILC.m
@@ -1,4 +1,4 @@
-% this function is the implementation of the paper [18], the parameters are tuned to achieve the best performance
+% this function is the implementation of the paper [18]
 function [U, E_SPSA_set_norm] = SPSA1ILC(R, U_ini, m, N)
 aj = 1;
 cj = 1;
